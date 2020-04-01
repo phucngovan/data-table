0423a888b3997e3d4017818d7e2bbe43e897f336
phucngovan <phucdepzai0712@gmail.com>
2020-04-01 22:55:14 +0700

    UI 80%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderTableComponent } from './header-table/header-table.component';
 import {AlertModule} from 'ngx-bootstrap';
 import {TooltipModule} from 'ngx-bootstrap';
-import { from } from 'rxjs';
-
+import {BsDropdownModule} from 'ngx-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +16,10 @@ import { from } from 'rxjs';
     BrowserModule,
     AppRoutingModule,
     AlertModule.forRoot(),
-    TooltipModule.forRoot()
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header-table/header-table.component.ts
+++ src/app/header-table/header-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header-table',
@@ -6,12 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-table.component.scss']
 })
 export class HeaderTableComponent implements OnInit {
-  public showAdvanced : boolean = false;
+  public showAdvanced = false;
+  public datas = [
+    {
+      id: 2,
+      imei: '359647090228276',
+      name: 'OB22 - Alex - BMV',
+      userName: 'developer',
+      typeName: 'OB22',
+      simno: '0862713692',
+      serviceExpire: '2020-10-24 07:23:10',
+      warrantyExpiredDate: '2020-10-24 07:23:10'
+    },
+    {
+      id: 3,
+      imei: '359647090228276',
+      name: 'OB22 - Alex - BMV',
+      userName: 'developer',
+      typeName: 'OB22',
+      simno: '0862713692',
+      serviceExpire: '2020-10-24 07:23:10',
+      warrantyExpiredDate: '2020-10-24 07:23:10'
+    },
+    {
+      id: 4,
+      imei: '359647090228276',
+      name: 'OB22 - Alex - BMV',
+      userName: 'developer',
+      typeName: 'OB22',
+      simno: '0862713692',
+      serviceExpire: '2020-10-24 07:23:10',
+      warrantyExpiredDate: '2020-10-24 07:23:10'
+    },
+    {
+      id: 5,
+      imei: '359647090228276',
+      name: 'OB22 - Alex - BMV',
+      userName: 'developer',
+      typeName: 'OB22',
+      simno: '0862713692',
+      serviceExpire: '2020-10-24 07:23:10',
+      warrantyExpiredDate: '2020-10-24 07:23:10'
+    },
+    {
+      id: 6,
+      imei: '359647090228276',
+      name: 'OB22 - Alex - BMV',
+      userName: 'developer',
+      typeName: 'OB22',
+      simno: '0862713692',
+      serviceExpire: '2020-10-24 07:23:10',
+      warrantyExpiredDate: '2020-10-24 07:23:10'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
   resetFilter() {
   }
 
